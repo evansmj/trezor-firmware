3692e785f13ad29dafba71f889ae7b6fec56b641
Martin Milata <martin@martinmilata.cz>
2026-07-16 01:59:35 +0200

    fix(core/translations): build version handling
    [no changelog]

--- tools/bump-version.py
+++ tools/bump-version.py
@@ -109,8 +109,8 @@ def cli(project: str | Path, version: str) -> None:
         )
         if project.match("core/embed/projects/firmware"):
             # also bump language JSONs
-            subprocess.check_call(
-                ["python", project.parents[2] / "translations" / "cli.py", "gen"]
+            subprocess.run(
+                ["python", project.parents[2] / "translations" / "cli.py", "version", "--reset"]
             )
         if project.match("core/embed/projects/prodtest"):
             # refresh error_codes.json
